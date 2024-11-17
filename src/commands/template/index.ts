aeee18f0fe61ec7246b7d5c97d0f46a478ab92b9
Topskys <2477350343@qq.com>
2024-11-17 14:48:37 +0800

    refactor: 重构添加删除模板命令

--- src/commands/template/index.ts
+++ src/commands/template/index.ts
@@ -1,33 +1,61 @@
 import ora from "ora";
-import path from 'path';
-import * as fs from 'fs-extra';
+import path from "path";
+import * as fs from "fs-extra";
+import { readTemplateSync } from "@/utils";
+import chalk from "chalk";
 
-const templatePath = path.resolve(__dirname, '../../templates.json');
+const templatePath = path.resolve(__dirname, "../../templates.json");
 
 /**
- * add命令处理函数
+ * 添加模板操作命令处理函数
+ *
+ * @param templateName 模板名称
+ * @param templateUrl 模板URL
+ * @param options 其他选项
+ * @returns 无返回值
+ * @throws 如果添加失败，则抛出异常
  */
-export const addAction = async (templateName: string, templateUrl: string, options) => {
-    const spinner = ora('Updating template...');
-    spinner.start();
-    const templates = JSON.parse(fs.readFileSync(templatePath, { encoding: 'utf-8' }));
-    templates[templateName.trim()] = templateUrl.trim();
-    await fs.outputFile(templatePath, JSON.stringify(templates, null, 2));
-    spinner.succeed('Updated successfully!');
-    process.exit();
-}
+export const addAction = async (
+  templateName: string,
+  templateUrl: string,
+  options
+) => {
+  try {
+    if (!fs.existsSync(templatePath)) {
+      fs.outputFileSync(templatePath, "{}");
+    }
+    const templateObj = readTemplateSync() as Record<string, string>;
+    templateObj[templateName.trim()] = templateUrl.trim();
+    fs.writeJSONSync(templatePath, templateObj, {
+      encoding: "utf-8",
+    });
+    console.log(chalk.green("Added successfully!"));
+  } catch (e) {
+    console.log(chalk.red("Add failed!"));
+  }
+  process.exit();
+};
 
 /**
- * remove命令处理函数
+ * 从模板对象中删除指定模板名称对应的模板命令处理函数
+ *
+ * @param templateName 模板名称
+ * @param options 其他选项（可选）
  */
 export const removeAction = async (templateName: string, options) => {
-    const spinner = ora('Removing template...');
-    spinner.start();
-    const templates = JSON.parse(fs.readFileSync(templatePath, { encoding: 'utf-8' }));
-    if (templates[templateName.trim()]) {
-        delete templates[templateName.trim()];
-        await fs.outputFile(templatePath, JSON.stringify(templates, null, 2));
+  try {
+    if (!fs.existsSync(templatePath)) {
+      fs.outputFileSync(templatePath, "{}");
+      process.exit();
     }
-    spinner.succeed('Removed successfully!');
-    process.exit();
-}
+    const templateObj = readTemplateSync() as Record<string, string>;
+    delete templateObj[templateName.trim()];
+    fs.writeJSONSync(templatePath, templateObj, {
+      encoding: "utf-8",
+    });
+    console.log(chalk.green("Removed successfully!"));
+  } catch (e) {
+    console.log(chalk.red("Remove failed!"));
+  }
+  process.exit();
+};
